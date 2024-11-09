eafabd2e95a3b17480ae50b394a14135600dfa25
akhkim <andrewkihyun@gmail.com>
2024-11-09 17:13:18 -0500

    solved conflicts

--- src/main/java/interface_adapter/results/ResultsState.java
+++ src/main/java/interface_adapter/results/ResultsState.java
@@ -1,23 +1,5 @@
 package interface_adapter.results;
 
-import entity.Game;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ResultsState {
-    private List<Game> games = new ArrayList<>();
     
-    public ResultsState(ResultsState copy) {
-        this.games = new ArrayList<>(copy.games);
-    }
-
-    public ResultsState() {}
-
-    public List<Game> getGames() {
-        return games;
-    }
-
-    public void setGames(List<Game> games) {
-        this.games = games;
-    }
 }
