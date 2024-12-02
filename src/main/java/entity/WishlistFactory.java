52e3e18fca5932f117d810778fdf443e5d9aa103
Frederick Chen <108845209+Pancraes@users.noreply.github.com>
2024-12-02 16:34:22 -0500

    Update WishlistFactory.java

--- src/main/java/entity/WishlistFactory.java
+++ src/main/java/entity/WishlistFactory.java
@@ -1,15 +1,15 @@
-//package entity;
-//
-//import org.json.JSONArray;
-//import org.json.JSONObject;
-//
-//public class WishlistFactory {
-//    public Wishlist create(JSONArray jsonWishlist) {
-//        Wishlist wishlist = Wishlist.create();
-//        for (int i = 0; i < jsonWishlist.length(); i++) {
-//            JSONObject gameJson = jsonWishlist.getJSONObject(i);
-//            wishlist.addGame(new GameFactory().create(gameJson));
-//        }
-//        return wishlist;
-//    }
-//}
+package entity;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WishlistFactory {
+    public Wishlist create(JSONArray jsonWishlist) {
+        Wishlist wishlist = Wishlist.create();
+        for (int i = 0; i < jsonWishlist.length(); i++) {
+            JSONObject gameJson = jsonWishlist.getJSONObject(i);
+            wishlist.addGame(new GameFactory().create(gameJson));
+        }
+        return wishlist;
+    }
+}
