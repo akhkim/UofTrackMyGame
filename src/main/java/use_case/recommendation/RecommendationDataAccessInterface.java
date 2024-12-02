b12e0b023b5af0bfba78efd9c25ac9c1a188ec3f
John Zhang <23zhanyuc@gmail.com>
2024-12-01 19:12:54 -0500

    Implement recommendation use case classes

---
+++ src/main/java/use_case/recommendation/RecommendationDataAccessInterface.java
@@ -0,0 +1,8 @@
+package use_case.recommendation;
+
+import use_case.search.GameSearchDataAccessInterface;
+import use_case.wishlist.WishlistDataAccessInterface;
+
+public interface RecommendationDataAccessInterface extends WishlistDataAccessInterface, GameSearchDataAccessInterface {
+
+}
