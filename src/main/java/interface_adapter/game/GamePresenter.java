46f1f66d6f09d3c961456c0e9ecc0d45a27b1b74
Frederick Chen <108845209+Pancraes@users.noreply.github.com>
2024-12-02 16:34:47 -0500

    Update GamePresenter.java

--- src/main/java/interface_adapter/game/GamePresenter.java
+++ src/main/java/interface_adapter/game/GamePresenter.java
@@ -1,23 +1,33 @@
 package interface_adapter.game;
 
+import interface_adapter.ViewManagerModel;
 import use_case.game.GameOutputBoundary;
 import use_case.game.GameOutputData;
 
 public class GamePresenter implements GameOutputBoundary {
-    private final GameViewModel viewModel;
+    private final GameViewModel gameViewModel;
+    private final ViewManagerModel viewManagerModel;
 
-    public GamePresenter(GameViewModel viewModel) {
-        this.viewModel = viewModel;
+    public GamePresenter(GameViewModel gameViewModel,
+                         ViewManagerModel viewManagerModel) {
+        this.gameViewModel = gameViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
 
     @Override
     public void presentGameDetails(GameOutputData outputData) {
-        // Update the view model with game details
-        viewModel.updateGameDetails(
-                outputData.getTitle(),
-                outputData.getSalePrice(),
-                outputData.getMetacriticScore(),
-                outputData.getDealRating()
-        );
+        final GameState gameState = gameViewModel.getState();
+        gameState.setGame(outputData.getGame());
+        this.gameViewModel.setState(gameState);
+        this.gameViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(gameViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    public void prepareFailView(String error) {
+        final GameState gameState = gameViewModel.getState();
+        gameState.setError(error);
+        gameViewModel.firePropertyChanged();
     }
 }
