aeb4258106826efc8cb77312fee204379ac303f0
Frederick Chen <108845209+Pancraes@users.noreply.github.com>
2024-12-02 16:31:21 -0500

    the next few commits will be putting the changes of main into my branch, for some reason i cant just merge from main, and the changes are not being shown so im doing it manually through github

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,26 +1,20 @@
-package app;
-
-import java.awt.CardLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-
-import entity.Game;
-import entity.GameFactory;
-import interface_adapter.ViewManagerModel;
-import interface_adapter.game.GameViewModel;
-import view.GameView;
-import view.ViewManager;
 
 public class Main {
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
         final JFrame application = appBuilder
-                                    .addGameSearchView()
-                                    .addResultsView()
-                                    .addWishlistView()
-                                    .build();
+                .addGameView()
+                .addGameUseCase()
+                .addGameSearchView()
+                .addResultsView()
+                .addGameSearchUseCase()
+                .addResultsUseCase()
+                .addHomeUseCase()
+                .addRecommendationUseCase()
+                .build();
+
         application.pack();
         application.setVisible(true);
     }
