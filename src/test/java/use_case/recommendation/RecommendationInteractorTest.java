0b97616bf92b5229ff4936d5b53182ffb5914361
John Zhang <23zhanyuc@gmail.com>
2024-12-02 19:28:28 -0500

    Add and improve recommendation test cases

--- src/test/java/use_case/recommendation/RecommendationInteractorTest.java
+++ src/test/java/use_case/recommendation/RecommendationInteractorTest.java
@@ -2,18 +2,23 @@ package use_case.recommendation;
 
 import data_access.DataAccess;
 import entity.Game;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import use_case.search.GameSearchDataAccessInterface;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RecommendationInteractorTest {
+    Game game;
+    RecommendationInputData inputData;
 
-    @Test
-    void outputSizeTest() {
-        Game game = new Game(
+    @BeforeEach
+    void init() {
+        game = new Game(
                 "220",
                 "Fallout: A Post Nuclear Role Playing Game",
                 "2.49",
@@ -28,47 +33,99 @@ public class RecommendationInteractorTest {
                 "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/38400/capsule_sm_120.jpg?t=1572025441",
                 "https://store.steampowered.com"
         );
-        final double PRICE = 2.49;
+        inputData = new RecommendationInputData(game);
+    }
 
-        RecommendationInputData inputData = new RecommendationInputData(game);
+
+    @Test
+    void successTest() {
         RecommendationOutputBoundary presenter = outputData -> {
             ArrayList<Game> games = outputData.getGames();
             assertEquals(9, games.size());
-        };
-
-        RecommendationInputBoundary interactor  = new RecommendationInteractor(presenter, new DataAccess());
-        interactor.execute(inputData);
-    }
-
-    @Test
-    void similarPricesTest() {
-        Game game = new Game(
-                "220",
-                "Fallout: A Post Nuclear Role Playing Game",
-                "2.49",
-                "9.99",
-                "1",
-                "75.075075",
-                "0",
-                "Overwhelmingly Positive",
-                "95",
-                "13739",
-                "3.1",
-                "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/38400/capsule_sm_120.jpg?t=1572025441",
-                "https://store.steampowered.com"
-        );
-        final double PRICE = 2.49;
-        final double THRESHOLD = 1.0;
-
-        RecommendationInputData inputData = new RecommendationInputData(game);
-        RecommendationOutputBoundary presenter = outputData -> {
-            ArrayList<Game> games = outputData.getGames();
             for (Game game1 : games) {
-                assertTrue(Math.abs(PRICE - Double.parseDouble(game1.getSalePrice())) < THRESHOLD);
+                assertEquals(Double.parseDouble(game.getSalePrice()), Double.parseDouble(game1.getSalePrice()));
             }
         };
 
-        RecommendationInputBoundary interactor  = new RecommendationInteractor(presenter, new DataAccess());
+        GameSearchDataAccessInterface dataAccess = new DataAccess() {
+            @Override
+            public String searchByFilters(String upperPrice, String lowerPrice, String metacritic, String onSale, String sortBy, String desc) {
+                final double[] prices = {2.49, 2.49, 2.49, 2.49, 2.49, 2.49, 2.49, 2.49, 2.49, 2.49, 2.49};
+                JSONArray array = new JSONArray();
+                for (double price : prices) {
+                    if (price <= Double.parseDouble(upperPrice) && price >= Double.parseDouble(lowerPrice)) {
+                        JSONObject game = new JSONObject();
+                        game.put("salePrice", Double.toString(price));
+                        array.put(game);
+                    }
+                }
+                return array.toString(4);
+            }
+        };
+
+        RecommendationInputBoundary interactor = new RecommendationInteractor(presenter, dataAccess);
+        interactor.execute(inputData);
+    }
+
+
+    @Test
+    void successIncreaseDeltaTest() {
+        RecommendationOutputBoundary presenter = outputData -> {
+            ArrayList<Game> games = outputData.getGames();
+            assertEquals(9, games.size());
+            for (Game game1 : games) {
+                assertEquals(Double.parseDouble(game.getSalePrice()), Double.parseDouble(game1.getSalePrice()), 0.02);
+            }
+        };
+
+        GameSearchDataAccessInterface dataAccess = new DataAccess() {
+            @Override
+            public String searchByFilters(String upperPrice, String lowerPrice, String metacritic, String onSale, String sortBy, String desc) {
+                final double[] prices = {2.49, 2.50, 2.48, 2.48, 2.48, 2.48, 2.49, 2.49, 2.50, 2.49};
+                JSONArray array = new JSONArray();
+                for (double price : prices) {
+                    if (price <= Double.parseDouble(upperPrice) && price >= Double.parseDouble(lowerPrice)) {
+                        JSONObject game = new JSONObject();
+                        game.put("salePrice", Double.toString(price));
+                        array.put(game);
+                    }
+                }
+                return array.toString(4);
+            }
+        };
+
+        RecommendationInputBoundary interactor = new RecommendationInteractor(presenter, dataAccess);
+        interactor.execute(inputData);
+    }
+
+
+    @Test
+    void successMaxDeltaTest() {
+        RecommendationOutputBoundary presenter = outputData -> {
+            ArrayList<Game> games = outputData.getGames();
+            assertEquals(2, games.size());
+            for (Game game1 : games) {
+                assertEquals(Double.parseDouble(game.getSalePrice()), Double.parseDouble(game1.getSalePrice()), 0.02);
+            }
+        };
+
+        GameSearchDataAccessInterface dataAccess = new DataAccess() {
+            @Override
+            public String searchByFilters(String upperPrice, String lowerPrice, String metacritic, String onSale, String sortBy, String desc) {
+                final double[] prices = {2.49, 2.49, 10.49, 0.00};
+                JSONArray array = new JSONArray();
+                for (double price : prices) {
+                    if (price <= Double.parseDouble(upperPrice) && price >= Double.parseDouble(lowerPrice)) {
+                        JSONObject game = new JSONObject();
+                        game.put("salePrice", Double.toString(price));
+                        array.put(game);
+                    }
+                }
+                return array.toString(4);
+            }
+        };
+
+        RecommendationInputBoundary interactor = new RecommendationInteractor(presenter, dataAccess);
         interactor.execute(inputData);
     }
 }
