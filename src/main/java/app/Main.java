19c7c76824dba46d958cc7aaca79c325303d390a
kerri-wei <kkerriwei@gmail.com>
2024-12-03 13:10:27 -0500

    checkstyle in DataAccess and Main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,22 @@
+
 package app;
 
 import javax.swing.JFrame;
 
+/**
+ * Entry point of UofTrackMyGame.
+ */
 public class Main {
+
+    /**
+     * Entry point for the UofTrackMyGame application.
+     * <p>
+     * Initializes the {@link AppBuilder}, adds views and use cases to the application,
+     * and builds the main window which is then displayed.
+     * </p>
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
         final JFrame application = appBuilder
